8232f5872d81aedb0e4b817a452eccb82d4cb7af
Khalid Talakshi <khalid.talakshi@bluejays.com>
2025-03-29 15:48:27 -0400

    updated lsp config with new lsps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -19,6 +19,7 @@ return {
 		config = function()
 			local lsp_config = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			lsp_config.lua_ls.setup({
 				capabilities = capabilities,
 			})
@@ -31,11 +32,37 @@ return {
 			lsp_config.eslint.setup({
 				capabilities = capabilities,
 			})
+			lsp_config.jinja_lsp.setup({
+				capabilities = capabilities,
+				filetypes = { "jinja" },
+			})
+			lsp_config.pyright.setup({
+				capabilities = capabilities,
+			})
+			lsp_config.html.setup({
+				capabilities = capabilities,
+				filetypes = { "html", "jinja" },
+			})
+			lsp_config.tailwindcss.setup({
+				capabilities = capabilities,
+				filetypes = { "typescriptreact" },
+			})
+			lsp_config.sqlls.setup({
+				capabilities = capabilities,
+			})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover({ border = "rounded" })
+			end, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gc", vim.lsp.buf.declaration, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+			vim.diagnostic.config({
+				float = {
+					border = "rounded",
+				},
+			})
 		end,
 	},
 }
