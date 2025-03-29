f12607ea87519952c04208da10d4b0841ee650a7
Khalid Talakshi <khalid.talakshi@bluejays.com>
2025-03-29 15:49:56 -0400

    reformatted neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,16 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
+  lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<leader>nf', ':Neotree filesystem reveal left toggle<CR>', {})
-    vim.keymap.set('n', '<leader>ng', ':Neotree git_status reveal left toggle<CR>', {}) 
-  end
+    vim.keymap.set("n", "<leader>nf", ":Neotree filesystem reveal left toggle<CR>", {})
+    vim.keymap.set("n", "<leader>ng", ":Neotree git_status reveal left toggle<CR>", {})
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  end,
 }
-
