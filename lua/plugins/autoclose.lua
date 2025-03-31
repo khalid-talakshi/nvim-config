83ea19f7af682ff11cc2d4abc80aa2a1cf6c5682
Khalid Talakshi <khalid.talakshi@bluejays.com>
2025-03-31 09:02:49 -0400

    disable autoclose for markdown files

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,6 +1,12 @@
 return {
   "m4xshen/autoclose.nvim",
   config = function()
-    require("autoclose").setup()
+    require("autoclose").setup({
+      options = {
+        disabled_filetypes = {
+          "markdown",
+        },
+      },
+    })
   end,
 }
