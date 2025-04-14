a6b802188696df79cd39426fa33a4277e590a422
Khalid Talakshi <khalid.talakshi@bluejays.com>
2025-04-14 09:54:21 -0400

    added support for debugging/dap

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,34 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap-python",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("dap-python").setup()
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    vim.keymap.set('n', '<Leader>do', dap.step_over, {})
+    vim.keymap.set('n', '<Leader>di', dap.step_into, {})
+    vim.keymap.set('n', '<Leader>du', dap.step_out, {})
+  end,
+}
