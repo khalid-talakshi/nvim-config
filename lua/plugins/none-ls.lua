d9ad033bcb0045c989498d031fca8fa9c3fab54a
Khalid Talakshi <khalid.talakshi@bluejays.com>
2025-04-14 09:54:11 -0400

    format update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,22 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier.with({
-          diagnostic_format = "[prettier] #{m}\n(#{c})",
-        }),
-        require("none-ls.diagnostics.ruff"),
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier.with({
+					diagnostic_format = "[prettier] #{m}\n(#{c})",
+				}),
+				null_ls.builtins.formatting.sqlfluff,
+				require("none-ls.diagnostics.ruff"),
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    vim.keymap.set("v", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("v", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
